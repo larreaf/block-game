3aed5926d19ca107d79fabb257685ec5a005f78f
larreaf <fedeml.95@gmail.com>
2023-11-21 13:34:57 -0300

    Fixed touch movement precision

--- block-game/touch_movement.js
+++ block-game/touch_movement.js
@@ -1,8 +1,9 @@
 import movement from "./game-logic/movement"
 import { game } from "./game"
+import { BLOCK_SIZE } from "./constants/gameConstants"
 
-const horizontalTouchThreshold = 2
-const verticalTouchThreshold = 4
+const horizontalTouchThreshold = BLOCK_SIZE
+const verticalTouchThreshold = BLOCK_SIZE/2
 
 let touchStartX, touchStartY
 
@@ -24,21 +25,32 @@ export const touchmoveEventHandler = (e) => {
   
     const deltaX = touchEndX - touchStartX;
     const deltaY = touchEndY - touchStartY;
-  
+
+    // paintDotDebug(touchEndX, touchEndY)
+
     // You can adjust these threshold values for smoother movement
     if (Math.abs(deltaX) > horizontalTouchThreshold) {
       if (deltaX > 0)
         movement.moveRight(game) // Swipe right
       else
         movement.moveLeft(game) // Swipe left        
+      touchStartX = touchEndX;
+      
     }
     if (
       Math.abs(deltaY) > verticalTouchThreshold &&
       deltaY > 0) {
       game.score++
       movement.moveDown(game) // Swipe down
+      touchStartY = touchEndY;
     }
-  
-    touchStartX = touchEndX;
-    touchStartY = touchEndY;
   }
+
+function paintDotDebug(touchEndX, touchEndY){
+  const dot = document.createElement('div');
+  dot.classList.add("dot");
+  dot.style.left = `${touchEndX}px`;
+  dot.style.top = `${touchEndY}px`;
+  dot.id = Date.now();
+  document.body.append(dot);
+}
